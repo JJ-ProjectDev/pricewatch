d9d2973b3700abf78f49af2b0df92b627e551b85
joaquinrodes <joaquinrodes.dev@gmail.com>
2026-09-05 23:46:08 +0100

    style: implemented theme variables and dm mono for headings

--- services/web/src/main.tsx
+++ services/web/src/main.tsx
@@ -1,4 +1,6 @@
 import './styles.css'
+import '@fontsource/dm-mono/400.css'
+import '@fontsource/dm-mono/500.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
