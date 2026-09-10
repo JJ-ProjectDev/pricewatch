aeafda9d1cb2cecd4214ad4dae8434bf0cf32562
joaquinrodes <joaquinrodes.dev@gmail.com>
2026-09-10 20:33:04 +0100

    fix: clicking watchbutton on product card navigates to product detail instead of toggling watch and nested watchlist as protected route

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -19,9 +19,8 @@ export default function App() {
         <Route path="/products/:id" element={<ProductDetailPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/watchlist" element={<WatchlistPage />} />
-        //TODO return watchlist to be nested as a protected route
         <Route element={<ProtectedRoute />}>
+          <Route path="/watchlist" element={<WatchlistPage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Route>
