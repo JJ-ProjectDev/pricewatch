c05b330f43f8b83ce065f7251d6ecf303a2d23f3
joaquinrodes <joaquinrodes.dev@gmail.com>
2026-09-10 20:40:21 +0100

    fix: remove background wave for performance

--- services/web/src/components/layout/Layout.tsx
+++ services/web/src/components/layout/Layout.tsx
@@ -1,10 +1,9 @@
 import Navbar from './Navbar'
-import BackgroundWave from './BackgroundWave'
+
 import { Outlet } from 'react-router-dom'
 export default function Layout() {
   return (
     <>
-      <BackgroundWave />
       <Navbar />
       <main className="px-4 md:px-6 lg:px-50 py-10">
         <Outlet />
